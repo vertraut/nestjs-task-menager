4ecd04b8f70bb6be81c9b610c928589e3c3cbe91
Vadym <v.usov@mobidev.biz>
2025-03-23 23:59:39 +0100

    fixes the issue EntityRepository is deprecated typeorm

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,5 +1,10 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
 import { Task } from './task.entity';
 
-@EntityRepository(Task)
-export class TasksRepository extends Repository<Task> {}
+@Injectable()
+export class TasksRepository extends Repository<Task> {
+  constructor(dataSource: DataSource) {
+    super(Task, dataSource.createEntityManager());
+  }
+}
