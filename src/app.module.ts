32aa9995f0f635c08a9f6959fc083ad729d69e9a
Vadym <v.usov@mobidev.biz>
2025-03-26 23:17:29 +0100

    adds supporting env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TasksModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
