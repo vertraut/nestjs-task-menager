94a305de1762563b9c14461f6cf89128d9a26671
Vadym <v.usov@mobidev.biz>
2025-03-26 11:29:52 +0100

    adds auth/signup endpoint and logic to create user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly usersRepository: UsersRepository) {}
+
+  signUp(authCredentialDto: AuthCredentialsDto): Promise<void> {
+    return this.usersRepository.createUser(authCredentialDto);
+  }
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class UsersRepository extends Repository<User> {
   constructor(dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(authCredentialDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+    const user = this.create({ username, password });
+    await this.save(user);
   }
 }
